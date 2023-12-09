095b4eb9ea2ee1463d256ed94508bad65c9a5d48
Puskar-Roy <puskarroy600@gmail.com>
2023-12-09 16:55:31 +0530

    feat : vercel config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gin-mongo-api/src/database"
 	"gin-mongo-api/src/routes"
+	"log"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/secure"
 	limits "github.com/gin-contrib/size"
@@ -12,9 +13,10 @@ import (
 func main() {
 	router := gin.Default()
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://google.com"}
-	router.Use(limits.RequestSizeLimiter(10))
-	database.ConnectDB()
+	config.AllowOrigins = []string{"*"}
+	router.Use(limits.RequestSizeLimiter(100))
+	
+	database.ConnectDB(); 
 	router.Use(secure.New(secure.Config{
 		FrameDeny:             true,
 		ContentTypeNosniff:    true,
@@ -22,5 +24,8 @@ func main() {
 		ContentSecurityPolicy: "default-src 'self'",
 	}))
 	routes.UserRoute(router)
-	router.Run(":6000")
+
+	if err := router.Run(":6000"); err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
