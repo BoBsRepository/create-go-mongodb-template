2eafb9d05a2314ad1848388858e08912a87d1b93
Puskar-Roy <puskarroy600@gmail.com>
2023-12-09 12:45:14 +0530

    feat : cors , secure and limit

--- go.mod
+++ go.mod
@@ -8,6 +8,9 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/cors v1.5.0 // indirect
+	github.com/gin-contrib/secure v0.0.1 // indirect
+	github.com/gin-contrib/size v0.0.0-20230212012657-e14a14094dc4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"gin-mongo-api/src/database"
 	"gin-mongo-api/src/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/secure"
+	limits "github.com/gin-contrib/size"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://google.com"}
+	router.Use(limits.RequestSizeLimiter(10))
 	database.ConnectDB()
+	router.Use(secure.New(secure.Config{
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self'",
+	}))
 	routes.UserRoute(router)
 	router.Run(":6000")
 }
